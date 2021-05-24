716098d2aca01690ed3a088f4b139b3b1196d34e
Katerina-ops-18 <61978136+Katerina-ops-18@users.noreply.github.com>
2021-05-24 13:44:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // minimum terminal window height = 6
+            // DO NOT resize after launching
+            // kill with ^C
+            new RunChains(-1).Run();
+            // the argument has to be a positive integer
+            // -1 means occupy every column
+        }
+    }
+}
